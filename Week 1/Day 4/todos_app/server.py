7aed87aa729604947bd9b174e776593d99f2c9bb
Alfredo08 <alfredosv08@gmail.com>
2023-04-06 11:03:16 -0600

    Routes

---
+++ Week 1/Day 4/todos_app/server.py
@@ -0,0 +1,30 @@
+from flask import Flask
+
+app = Flask( __name__ )
+
+@app.route( "/home", methods=["GET"] )
+def hello_class():
+    return "Hey there class of Python/Flask April 2023!"
+
+@app.route( "/info", methods=["GET"] )
+def hello_class_with_info():
+    message = "I hope you enjoy working with Flask."
+    return f"Hey class {message}"
+
+@app.route( "/hello/<string:first_name>/<string:last_name>", methods=["GET"] )
+def greet_someone( first_name, last_name ):
+    print( f"Hey there {first_name} {last_name}" )
+    return f"<h1> Welcome back {first_name} {last_name} </h1>"
+
+@app.route( "/students", methods=["GET"] )
+def display_list_of_students():
+    student_list = [ "Alex", "Martha", "Roger", "Julie" ]
+    result = ""
+
+    for student in student_list:
+        result += f"<p> {student} </p>"
+    return f"<h1> The students of this class </h1> {result}"
+
+
+if __name__ == "__main__":
+    app.run( debug = True, port = 5001 )
