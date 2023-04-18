cb26c4ed7e2037e7562712368bebc1ff8c360a42
Alfredo08 <alfredosv08@gmail.com>
2023-04-18 15:19:48 -0600

    Finished belt review

--- Week 3/Day 2/book_club_app/flask_app/models/book_model.py
+++ Week 3/Day 2/book_club_app/flask_app/models/book_model.py
@@ -32,25 +32,64 @@ class Book:
         for row in results:
             current_book = cls( row )
             book_user = {
+                **row,
                 "created_at" : row["u.created_at"],
                 "updated_at" : row["u.updated_at"],
                 "id" : row["u.id"],
-                **row
             }
             current_book.user = user_model.User( book_user )
             list_of_books.append( current_book )
         return list_of_books
     
+    @classmethod
+    def get_one_with_user( cls, data ):
+        query  = "SELECT * "
+        query += "FROM books b JOIN users u "
+        query += "ON b.user_id = u.id "
+        query += "WHERE b.id = %(id)s;"
+        result = connectToMySQL( DATABASE ).query_db( query, data )
+        row = result[0]
+        current_book = cls( row )
+        book_user = {
+            **row,
+            "created_at" : row["u.created_at"],
+            "updated_at" : row["u.updated_at"],
+            "id" : row["u.id"]
+        }
+        current_book.user = user_model.User( book_user )
+        return current_book
+
+    @classmethod
+    def delete_one( cls, data ):
+        query  = "DELETE FROM books "
+        query += "WHERE id = %(id)s;"
+        return connectToMySQL( DATABASE ).query_db( query, data )
+    
+    @classmethod
+    def get_one( cls, data ):
+        query  = "SELECT * "
+        query += "FROM books "
+        query += "WHERE id = %(id)s;"
+        result = connectToMySQL( DATABASE ).query_db( query, data )
+        return cls( result[0] )
+    
+    @classmethod
+    def update_one( cls, data ):
+        query  = "UPDATE books "
+        query += "SET title = %(title)s, author = %(author)s, thoughts = %(thoughts)s, user_id = %(user_id)s "
+        query += "WHERE id = %(id)s;"
+        return connectToMySQL( DATABASE ).query_db( query, data )
+
     @staticmethod
     def validate_book( data ):
         is_valid = True
         if len( data["title"] ) == 0:
-            flash( "You must provide a title!.", "error_title" )
+            flash( "You must provide a title!", "error_title" )
             is_valid = False
         if len( data["author"] ) == 0:
-            flash( "You must provide the author!.", "error_author" )
+            flash( "You must provide the author!", "error_author" )
             is_valid = False
         if len( data["thoughts"] ) == 0:
-            flash( "You must provide your thoughts!.", "error_thoughts" )
+            flash( "You must provide your thoughts!", "error_thoughts" )
             is_valid = False
         return is_valid
